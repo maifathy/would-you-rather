e58078bf9db0bcf37a206c0b84c2836753bf360b
Mai Fathy <66532989+maifathy@users.noreply.github.com>
2020-09-11 15:09:03 +0200

    Update User.js

--- src/components/User.js
+++ src/components/User.js
@@ -2,55 +2,53 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Card } from 'react-bootstrap'
 
-class User extends Component {
-  render(){
-    const { user } = this.props
-    return(
-      <Card>
-        <Card.Body>
-          <Card.Img
-            className='card-image'
-            variant="top"
-            src={ user.avatarURL }
-            alt={`Avatar of ${ user.name }`}
-          />
+const User = (props) => {
+  const { user } = props
+  return(
+    <Card>
+      <Card.Body>
+        <Card.Img
+          className='card-image'
+          variant="top"
+          src={ user.avatarURL }
+          alt={`Avatar of ${ user.name }`}
+        />
 
-        <div className='card-text-leader'>
-          <span>
-            {user.name}
-          </span>
-          <br />
-          <br />
-          Answered questions
-          <span className='ml-5'></span>
-          <span className='ml-5'></span>
-          { Object.keys(user.answers).length }
-          <hr />
-          Created questions
-          <span className='ml-5'></span>
-          <span className='ml-5'></span>
-          <span className='ml-3 .ml-sm-0'></span>
-          { user.questions.length }
-        </div>
-        <Card className='card-score'>
-          <Card.Header
-            align='center'
-          >
-            Score
-          </Card.Header>
-          <Card.Body
-            align='center'
-          >
-            <div className='score' align='center'>
-              { Object.keys(user.answers).length + user.questions.length }
-            </div>
-          </Card.Body>
-        </Card>
+      <div className='card-text-leader'>
+        <span>
+          {user.name}
+        </span>
+        <br />
+        <br />
+        Answered questions
+        <span className='ml-5'></span>
+        <span className='ml-5'></span>
+        { Object.keys(user.answers).length }
+        <hr />
+        Created questions
+        <span className='ml-5'></span>
+        <span className='ml-5'></span>
+        <span className='ml-3 .ml-sm-0'></span>
+        { user.questions.length }
+      </div>
+      <Card className='card-score'>
+        <Card.Header
+          align='center'
+        >
+          Score
+        </Card.Header>
+        <Card.Body
+          align='center'
+        >
+          <div className='score' align='center'>
+            { Object.keys(user.answers).length + user.questions.length }
+          </div>
         </Card.Body>
-
       </Card>
-    )
-  }
+      </Card.Body>
+
+    </Card>
+  )
 }
 function mapStateToProps({ users }, { id }){
   const user = users[id]
